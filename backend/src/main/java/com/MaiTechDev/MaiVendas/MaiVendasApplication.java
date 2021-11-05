9bce87d4c125f958ac836b0dd0c435f260fe50bf
Mairavilhosa <maiheringer@gmail.com>
2021-11-04 22:47:58 -0300

    Created Projects

---
+++ backend/src/main/java/com/MaiTechDev/MaiVendas/MaiVendasApplication.java
@@ -0,0 +1,13 @@
+package com.MaiTechDev.MaiVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MaiTechDev/MaiVendas/MaiVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MaiTechDev.MaiVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
