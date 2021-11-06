f704235602b6e0e27c5fcf00dabc29894c30da6c
Mairavilhosa <maiheringer@gmail.com>
2021-11-06 19:59:50 -0300

    Pagination

---
+++ backend/src/main/java/com/MaiTechDev/MaiVendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.MaiTechDev.MaiVendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.MaiTechDev.MaiVendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
